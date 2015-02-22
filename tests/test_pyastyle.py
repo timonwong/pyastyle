aabdcab84a83147b7e0f879ee0eb6e5dc73a1e69
Timon Wong <timon86.wang@gmail.com>
2015-02-22 12:52:54 +0800

    Add `pyastyle.format` tests

--- tests/test_pyastyle.py
+++ tests/test_pyastyle.py
@@ -1,6 +1,7 @@
 import re
 
 import pyastyle
+import pytest
 
 
 def test_version_is_string():
@@ -11,3 +12,21 @@ def test_version_is_string():
 def test_version_is_numberic_like():
     ver = pyastyle.version()
     assert re.match(r'^(\d+\.){2,}(\d+\w?)$', ver)
+
+
+def test_format_simple_code():
+    code = '''\
+int main(void)
+{
+  }'''
+    expected = '''\
+int main(void)
+{
+}'''
+    assert expected == pyastyle.format(code, '--style=allman')
+
+
+def test_format_invalid_options():
+    with pytest.raises(pyastyle.error) as exc:
+        pyastyle.format('int main() {}', '--invalid-options-hereXXXXXX')
+    assert '[130]' in str(exc)
